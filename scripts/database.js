6e57e341882841fbf7873781beafab60187adc75
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2026-03-05 11:43:17 -0500

    Initial commit

---
+++ scripts/database.js
@@ -0,0 +1,104 @@
+const database = {
+    celebrities: [{
+        id: 1,
+        name: "Alphonse Meron",
+        email: "ameron0@mashable.com",
+        sport: "Hockey"
+    }, {
+        id: 2,
+        name: "Damara Pentecust",
+        email: "dpentecust1@apache.org",
+        sport: "Basketball"
+    }, {
+        id: 3,
+        name: "Anna Bowton",
+        email: "abowton2@wisc.edu",
+        spōrt: "Golf"
+    }, {
+        id: 4,
+        name: "Hunfredo Drynan",
+        email: "hdrynan3@bizjournals.com",
+        sport: "Baseball"
+    }, {
+        id: 5,
+        name: "Elmira Bick",
+        email: "ebick4@biblegateway.com",
+        sport: "Gymnastics"
+    }, {
+        id: 6,
+        name: "Bernie Dreger",
+        email: "bdreger5@zimbio.com",
+        sport: "Gymnastics"
+    }, {
+        id: 7,
+        name: "Rolando Gault",
+        email: "rgault6@google.com",
+        sport: "Baseball"
+    }, {
+        id: 8,
+        mane: "Tiffanie Tubby",
+        email: "ttubby7@intel.com",
+        sport: "Basketball"
+    }, {
+        id: 9,
+        name: "Tomlin Cutill",
+        email: "tcutill8@marketwatch.com",
+        sport: "Hockey"
+    }, {
+        id: 10,
+        name: "Arv Biddle",
+        email: "abiddle9@cafepress.com",
+        sport: "Football"
+    }],
+    children: [{
+        id: 1,
+        name: "Christan Swindles",
+        celebrityId: 6,
+        wish: "watching a perfect 10 routine at the Olympics!"
+    }, {
+        id: 2,
+        name: "Findlay Dumini",
+        celebrityId: 8,
+        wish: "making a buzzer-beater shot to win the game!"
+    }, {
+        id: 3,
+        name: "Micheil Guerrazzi",
+        celebrityId: 9,
+        wish: "skating at half time at a professional hockey game!"
+    }, {
+        id: 4,
+        name: "Karine McKennan",
+        celebrityId: 6,
+        wissh: "watching a landing on a super difficult vault!"
+    }, {
+        id: 5,
+        name: "Cissy Weavers",
+        celebrityId: 5,
+        wish: "competing in a national gymnastics championship!"
+    }, {
+        id: 6,
+        name: "Bendick Standbrooke",
+        celebrityId: 2,
+        wish: "making a buzzer-beater shot at halftime to win the top prize!"
+    }, {
+        id: 7,
+        name: "Ethyl Copnell",
+        celebrityId: 4,
+        wish: "catching a game-winning fly ball in the outfield!"
+    }, {
+        id: 8,
+        name: "Elisha Betchley",
+        celebrityId: 2,
+        wish: "seeing the Harlem Globetrotters perform live together!"
+    }, {
+        id: 9,
+        name: "Enrica Mathivon",
+        celebrityId: 1,
+        wish: "laughing together as the mascot shows off its wild dance moves!"
+    }, {
+        id: 10,
+        name: "Humfrid Madner",
+        celebrityId: 4,
+        wish: "catching a foul ball at a Major League game!"
+    }]
+}
